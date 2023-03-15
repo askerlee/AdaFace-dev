d8316099848cf976c10aede2b5d3cea75680a098
askerlee <shaohua@gmail.com>
2023-03-15 13:39:40 +0800

    TI test script

--- scripts/testall-ti.sh
+++ scripts/testall-ti.sh
@@ -7,10 +7,10 @@ set niter 1
 for i in (seq 25)
     set subject $subjects[$i]
     set prompt  "a z"
-    set ckptname  (ls -1 -rt logs|grep $subject|tail -1)
+    set ckptname  (ls -1 -rt logs|grep $subject-ti|tail -1)
     if test -z "$ckptname"
     	echo Unable to find the checkpoint of $subject
     	continue
     end
-    python3 scripts/stable_txt2img.py --config configs/stable-diffusion/v1-inference-ada.yaml --ckpt models/stable-diffusion-v-1-4-original/sd-v1-4-full-ema.ckpt --ddim_eta 0.0 --n_samples 8 --ddim_steps 100 --embedding_paths logs/$CKPT/checkpoints/embeddings_gs-4000.pt --gpu $GPU --prompt "a z" --scale 5 --n_iter $niter --outdir samples --indiv_subdir $subject
+    python3 scripts/stable_txt2img.py --config configs/stable-diffusion/v1-inference.yaml --ckpt models/stable-diffusion-v-1-4-original/sd-v1-4-full-ema.ckpt --ddim_eta 0.0 --n_samples 8 --ddim_steps 100 --embedding_paths logs/$ckptname/checkpoints/embeddings_gs-4000.pt --gpu $GPU --prompt "a z" --scale 5 --n_iter $niter --outdir samples-ti --indiv_subdir $subject
 end
