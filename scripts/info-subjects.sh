a1745a7b17436d94fee327e7dd11129091394d1a
askerlee <shaohua@gmail.com>
2023-04-05 17:52:40 +0800

    Tidy up info-subjects.sh

--- scripts/info-subjects.sh
+++ scripts/info-subjects.sh
@@ -5,9 +5,14 @@ set -l db_prompts   girl                girl                  girl             "
 set -l cls_tokens   girl                girl                  girl             man         woman          boy          man          girl             dog        man              cat          girl                man         woman         cat                  man         woman         girl          girl           man         man           girl         man                man                   girl
 set -l ada_prompts  "young girl woman"  "cyborg young girl"   "young girl"     "asian man" "black woman"  "young boy"  "asian man"  "young woman"    "pom dog"  "keanu cool man" "tabby cat"  "asian young girl"  "asian man" "asian woman" "black persian cat"  "white man" "asian woman" "young girl"  "indian girl"  "black man" "asian man"   "cute girl"  "french young man" "young handsome man"  "young girl zendaya"
 set -l ada_weights  "1 2 2"             "1 1 2"               "1 2"            "1 2"       "1 2"          "1 2"        "1 2"        "1 2"            "1 1"      "2 1 2"          "1 2"        "1 1 2"             "1 2"       "1 2"         "1 1 3"              "1 2"       "1 2"         "1 2"         "1 2"          "1 2"        "1 2"        "1 2"        "1 1 2"            "1 1 2"               "1 2 2"
+# broad_classes are all 1, i.e., humans/animals.
+set -l broad_classes  1                 1                     1                1           1              1            1            1                1          1                1            1                   1           1             1                    1           1             1             1              1           1             1            1                  1                     1           
+
+#                     objects    humans/animals    cartoon characters
+set -l lrs            3e-4       8e-4              3e-4
+
 # donnieyen jenniferlawrence jiffpom lilbub lisa michelleyeoh selenagomez smitrimandhana taylorswift zendaya
 set -l sel_set      4 8 9 11 12 14 18 19 22 25
-
 set -Ux subjects        $subjects
 set -Ux db_prompts      $db_prompts
 set -Ux ada_prompts     $ada_prompts
@@ -16,4 +21,5 @@ set -Ux cls_tokens      $cls_tokens
 # "instagram" for the main dataset, to focus on faces.
 set -Ux db_suffix       ", instagram"
 set -Ux sel_set         $sel_set
+set -Ux lrs             $lrs
 set -Ux data_folder     data
