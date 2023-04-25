8892db0302cc2935c58a30a8a51967d28e874305
askerlee <shaohua@gmail.com>
2023-04-25 20:02:44 +0800

    Addd option mix_stop_grad to DDPM

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -1152,16 +1152,21 @@ class LatentDiffusion(DDPM):
                         # i.e., subj_comps_emb and cls_comps_emb will be mixed.
                         subj_comps_emb_mix = mix_embeddings(subj_comps_emb, cls_comps_emb, 
                                                             c2_mix_weight=self.cls_prompt_mix_weight)
+                        # If mix_stop_grad, stop gradient on subj_comps_emb_mix, 
+                        # since it serves as the reference.
+                        # If we don't stop gradient on subj_comps_emb_mix, 
+                        # then chance is that subj_comps_emb_mix might be dominated by subj_comps_emb,
+                        # so that subj_comps_emb_mix will produce images similar as subj_comps_emb does.
+                        # mix_stop_grad will improve compositionality but reduce face similarity.
+                        mix_stop_grad = True
+                        if mix_stop_grad:
+                            subj_comps_emb_mix = subj_comps_emb_mix.detach()
                         # This copy of subj_comps_emb will be simply repeated at the token dimension
                         # to match the token number of the mixed (concatenated) embeddings.
                         subj_comps_emb = subj_comps_emb.repeat(1, 2, 1)
                         # Unmixed embeddings and mixed embeddings will be merged in one batch for guiding
                         # image generation and computing compositional mix loss.
-                        # Stop gradient on subj_comps_emb_mix, since it serves as the reference.
-                        # If we don't stop gradient on subj_comps_emb_mix, 
-                        # then chance is that subj_comps_emb_mix might be dominated by subj_comps_emb,
-                        # so that subj_comps_emb_mix will produce images similar as subj_comps_emb does.
-                        c_static_emb2  = torch.cat([subj_comps_emb, subj_comps_emb_mix.detach()], dim=0)
+                        c_static_emb2  = torch.cat([subj_comps_emb, subj_comps_emb_mix], dim=0)
 
                     elif self.do_ada_comp_delta_reg:
                         # Do ada composition delta loss in this iteration. 
