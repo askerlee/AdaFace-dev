d0a6af78144e9d6365aefb551886bc93b7cea8c6
askerlee <shaohua@gmail.com>
2023-05-23 14:13:08 +0800

    Make two.sh flexible

--- scripts/two.sh
+++ scripts/two.sh
@@ -1,28 +1,31 @@
-#!/bin/bash
+#!/usr/bin/fish
+set scriptname (status -f)
 
-if [ "$#" -lt 2 ];
-then
- echo "Usage: $0 file1 file2 ..."
+if test (count $argv) -lt 2
+ echo "Usage: $scriptname file1 file2 ..."
  exit
-fi
+end
 
-if [[ $1 =~ "/" ]]; then
-   folder=""
+if string match -eq "/" $argv[1] 
+   set folder ""
 else
-   folder="outputs/txt2img-samples"
-fi
+   set folder "samples-ada/"
+end
 
-feh  --scale-down -g 1600x200+5+30  "$folder$1" &
-feh  --scale-down -g 1600x200+0+250 "$folder$2" &
-if [ ! -z "$3" ]
-then
- feh  --scale-down -g 1600x200+0+470 "$folder$3" &
-fi
-if [ ! -z "$4" ]
-then
- feh  --scale-down -g 1600x200+0+690 "$folder$4" &
-fi
-if [ ! -z "$5" ]
-then
- feh  --scale-down -g 1600x200+0+910 "$folder$5" &
-fi
+# screen dimensions: 1600x1200
+set WIDTH 1600
+set HEIGHT 1200
+set max_images 5
+set padding 20
+set img_height (math (math $HEIGHT - 30) / $max_images - $padding)
+
+set start_idx (count $argv)
+set end_idx (math max 1, (math $start_idx - $max_images + 1))
+set h_offset $padding
+
+for img_idx in (seq $start_idx -1 $end_idx)
+  #echo $argv[$img_idx]
+  feh  --scale-down -g {$WIDTH}x200+0+{$h_offset}  "$folder$argv[$img_idx]" &
+  set h_offset (math $h_offset + $img_height)
+end 
+
