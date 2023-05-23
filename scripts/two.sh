d3e103908825a26593f07c34150e749dd790278f
askerlee <shaohua@gmail.com>
2023-05-23 14:47:39 +0800

    Improve two.sh

--- scripts/two.sh
+++ scripts/two.sh
@@ -6,11 +6,7 @@ if test (count $argv) -lt 1
  exit
 end
 
-if string match -eq "/" $argv[1] 
-   set folder ""
-else
-   set folder "samples-ada/"
-end
+set default_folder "samples-ada/"
 
 # screen dimensions: 1600x1200
 set WIDTH 1600
@@ -24,6 +20,14 @@ set end_idx (math max 1, (math $start_idx - $max_images + 1))
 set h_offset $padding
 
 for img_idx in (seq $start_idx -1 $end_idx)
+  set img_path $argv[$img_idx]
+  # test if img_path exists
+   if test -f $img_path
+      set folder ""
+   else
+      set folder $default_folder
+   end
+   
   #echo $argv[$img_idx]
   feh  --scale-down -g {$WIDTH}x200+0+{$h_offset}  "$folder$argv[$img_idx]" &
   set h_offset (math $h_offset + $img_height)
