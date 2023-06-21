61fb3d0abd61f0bbadbbb3c82c3cd7144b75a1e8
askerlee <shaohua@gmail.com>
2023-06-21 09:21:07 +0800

    Scale up attn loss by 10x

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -1857,7 +1857,8 @@ class LatentDiffusion(DDPM):
                     #                                               first_n_dims_to_flatten=2, 
                     #                                               ref_grad_scale=0)
                     loss_layer_subj_attn_distill = self.get_loss(attn_subj_delta, attn_mix_delta, mean=True)
-                    loss_subj_attn_distill += loss_layer_subj_attn_distill * attn_distill_layer_weight
+                    # L2 loss tends to be smaller than delta loss. So we scale it up by 10.
+                    loss_subj_attn_distill += loss_layer_subj_attn_distill * attn_distill_layer_weight * 10
 
                 use_subj_attn_as_spatial_weights = True
                 if use_subj_attn_as_spatial_weights:
