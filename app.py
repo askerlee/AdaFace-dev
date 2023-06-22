ad62bc1f896c8648dc8b4d66a300358c994616f1
jefflai0315 <jefflai0315@gmail.com>
2023-06-22 15:55:18 +0800

    seed and gpu webui

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ from scripts.stable_txt2img import main
 from webuiParamClass import DictI, DictT
 import random
 
-current_seed = random.randint(0,9999999)
-default_json = {"seed": current_seed}
-stats = gr.State(value=default_json)
+
 opt = DictI() # create an instance containing default param for Inference
 
 with open('webui-setting-config.yaml','r') as f: # open config file to load pre settings
@@ -29,7 +27,7 @@ def generate(*args):
     opt.ddim_eta = args[6]
     opt.n_samples = args[7]
     opt.dim_step = args[8]
-    opt.gpu = args[9]
+    opt.gpu = int(args[9])
     opt.embedding_paths = args[10]
     opt.H = args[11]
     opt.W = args[12]
@@ -46,38 +44,34 @@ def generate(*args):
     opt.broad_class = args[23]
     opt.clip_last_layer_skip_weight = args[24]
     opt.plms = args[25]
-    opt.laion400m = args[26]
-    opt.fixed_code = args[27]
+    opt.fixed_code = args[26]
+    opt.no_preview = args[27]
 
 
     if opt.init_img == '':
         opt.init_img = None
-    if opt.gpu: 
-        opt.gpu = 1
-    else:
-        opt.gpu = -1
+    if opt.seed == -1: 
+        opt.seed = random.randint(0,100000)
+        with open('webui-setting-config.yaml', 'r') as f:
+            settings = yaml.safe_load(f)
+        settings['seed'] = opt.seed
+        with open('webui-setting-config.yaml','w') as f:
+            yaml.dump(settings, f)
+            print(f'saved latest seed = {opt.seed} to webui-setting-config.yaml')
+        
   
     print(opt)
-    print(model,"value")
-    print(model,"info")
-
     img = main(opt)
-
-
-    # generate code
     return img
-random_num = gr.Slider(visible=False, value=-1)
-def compare(seed, random_num):
-    if seed != random_num:
-        return -1
 
-def random_num_gen():
-    #generate random number 
-    print("Generating random number")
-    #change seed slider value 
-    # return random.randint(0,9999999)
-    current_random_num = random.randint(0,9999999)
-    return current_random_num
+
+# def random_num_gen():
+#     #generate random number 
+#     print("Generating random number")
+#     #change seed slider value 
+#     # return random.randint(0,9999999)
+#     current_random_num = random.randint(0,9999999)
+#     return current_random_num
     
 
 with gr.Blocks() as demo:
@@ -102,6 +96,9 @@ with gr.Blocks() as demo:
             with gr.Column(scale=2) as col1:
                 prompt = gr.Textbox(lines=2, label="Prompt", placeholder="Enter a prompt...")
                 class_prompt = gr.Textbox(lines=2, label="Class Prompt", placeholder="Enter a class prompt...")
+                embedding_path_entries = os.listdir('subject-models/ada-delta')
+                embedding_path_entries = ['subject-models/ada-delta/' + path + '/embeddings_gs-4500.pt' for path in embedding_path_entries]
+                embedding_paths = gr.Dropdown (list(embedding_path_entries), label='Embedding Path', info='will add more later', multiselect=True)
                 with gr.Row() as row00:
                     with gr.Column():
                         config_entries = os.listdir('configs/stable-diffusion/')
@@ -112,7 +109,8 @@ with gr.Blocks() as demo:
                 with gr.Row() as row2:
                     ddim_eta = gr.Slider(minimum=0, maximum=1, value=0.0, label="Dimension", step=0.1)
                     dim_step = gr.Slider(minimum=0, maximum=250,value=50,  label="Dimension Step", step=1)
-                    gpu = gr.Checkbox(label="Use GPU", value=True)
+                    # gpu = gr.Checkbox(label="Use GPU", value=True)
+                    gpu = gr.Dropdown( [0,1,2,3,4,-1], label="GPU", value = '0' , info='GPU id; -1 for CPU')
                 with gr.Row() as row3:
                     with gr.Column():
                         H = gr.Slider(minimum=0, maximum=1000, value=512, label="Height", step=1)
@@ -129,17 +127,18 @@ with gr.Blocks() as demo:
                     n_repeat = gr.Slider(minimum=0, maximum=20, value=1, label="n_repeat", step=1)
                 with gr.Row() as row5:
                     with gr.Column(scale=0.8):
-                        seed = gr.Slider(minimum=0, maximum=100, value=42, label="Seed", step=1,interactive=True)
+                        seed = gr.Number(minimum=-1, maximum=100000, value=-1, label="Seed", step=1,interactive=True)
                         # seed.change(compare,[seed,random_num],seed)
-                        
-                    # with gr.Column(scale = 0.1):
-                    #     random_seed = gr.Button(value="🎲")
-                    #     random_seed.style(size='sm')
-                    #     # random_seed.click(random_num_gen)
-                    # # with gr.Column(scale=0.1):
-                    #     previous_seed = gr.Button(value="♻️")
-                    #     previous_seed.style(size='sm')
-                        # previous_seed.click()
+                        random_num = gr.Number(value = -1, visible = False)
+                    with gr.Column(scale = 0.1):
+                        random_seed = gr.Button(value="🎲")
+                        random_seed.style(size='sm')
+                        random_seed.click(lambda x: x, random_num, seed)
+                    # with gr.Column(scale=0.1):
+                        previous_seed = gr.Button(value="♻️")
+                        previous_seed.style(size='sm')
+                        stored_seed = gr.Number(value = opt.seed,visible = False)
+                        previous_seed.click(lambda x: x, stored_seed,seed)
                 with gr.Row():
                     precision = gr.Dropdown(['autocast', 'float32', 'float64'], label='Precision',value='autocast')
                 with gr.Row() as row6:
@@ -153,14 +152,11 @@ with gr.Blocks() as demo:
                     # skip_grid = gr.Checkbox(label="Skip Grid", info="Skip Grid")
                     plmse = gr.Checkbox(label="PLMSE")
                     # skip_save = gr.Checkbox(label="Skip Save", info="Skip Save")
-                    laion400m = gr.Checkbox(label="Laion400m")
+                    
                     fixed_code = gr.Checkbox(label="Fixed Code")
                     no_preview = gr.Checkbox(label="No Preview")
         
-                embedding_path_entries = os.listdir('subject-models/ada-delta')
-                embedding_path_entries = ['subject-models/ada-delta/' + path + '/embeddings_gs-4500.pt' for path in embedding_path_entries]
-                embedding_paths = gr.Dropdown (list(embedding_path_entries), label='Embedding Path', info='will add more later', multiselect=True)
-                # add 'subject-models/' to everyting in the list
+               # add 'subject-models/' to everyting in the list
 
             with gr.Column(scale=1.5) as col2:
                 #add image output 
@@ -169,31 +165,33 @@ with gr.Blocks() as demo:
                 # button2 = gr.Button(value="Close")
                 with gr.Row():
                     output = gr.Image(label="Output")
-                button1.click(generate, outputs=output, inputs=[prompt, class_prompt, config, model, scale, n_iters, ddim_eta, n_samples, dim_step, gpu, embedding_paths, H, W, C, f, bs, n_repeat, n_rows, seed, precision, subj_scale, ada_emb_weight, mask_weight, broad_class, clip_last_layer_skip_weight, plmse, laion400m, fixed_code, no_preview])
+                button1.click(generate, outputs=output, inputs=[prompt, class_prompt, config, model, scale, n_iters, ddim_eta, n_samples, dim_step, gpu, embedding_paths, H, W, C, f, bs, n_repeat, n_rows, seed, precision, subj_scale, ada_emb_weight, mask_weight, broad_class, clip_last_layer_skip_weight, plmse, fixed_code, no_preview])
                 # button2.click(close)
     with gr.Tab(label="Training") as tab0:
         gr.Markdown('work in progress')
         pass
     with gr.Tab(label="Settings"):
         gr.Markdown('work in progress')
+        with open('webui-setting-config.yaml','r') as config_settings:
+            settings = yaml.safe_load(config_settings)
         def apply_settings(*args):
-            setting = {}
             # change the setting values and save in a file so that the next time the app is opened, the settings are the same
-            setting['skip_save'] =  args[0]
-            setting['file_format'] = args[1]
-            setting['image_file_pattern'] = args[2]
-            setting['add_image_number'] = args[3]
-            setting['skip_grid'] =  args[4]
-            setting['file_format_grid'] = args[5]
-            setting['entended_info'] = args[6]
-            setting['from_file'] = args[7]
-            setting['init_img'] = args[8]
-            setting['calc_face_sim']= args[9]
-            setting['compare_with']= args[10]
+            # settings['skip_save'] =  args[0]
+            # settings['file_format'] = args[1]
+            # settings['image_file_pattern'] = args[2]
+            # settings['add_image_number'] = args[3]
+            # settings['skip_grid'] =  args[4]
+            # settings['file_format_grid'] = args[5]
+            # settings['entended_info'] = args[6]
+            # settings['from_file'] = args[7]
+            # settings['init_img'] = args[8]
+            # settings['calc_face_sim']= args[9]
+            # settings['compare_with']= args[10]
+            # settings['laion400m'] = args[11]
 
-            with open('config.yaml','w') as f:
-                yaml.dump(setting, f)
-                print('saved settings to config.yaml')
+            with open('webui-setting-config.yaml','w') as f:
+                yaml.dump(settings, f)
+                print('saved settings to webui-setting-config.yaml')
 
         with gr.Row():
             with gr.Column(scale=6):
@@ -202,24 +200,27 @@ with gr.Blocks() as demo:
                 restart_gradio = gr.Button(value='Reload UI', variant='primary', elem_id="settings_restart_gradio")
 
         with gr.TabItem("Saving images/grids", id="saving", elem_id="settings_tab_saving"):
-            save_img = gr.Checkbox(label="Skip to save images", info="Save images",value= opt['skip_save'])
-            file_format = gr.Text(label="File format", placeholder="jpg", info="File format to save images in")
-            image_file_pattern = gr.Text(label="Image file pattern", placeholder="image_{i}", info="File pattern to save images with")
-            add_image_number = gr.Checkbox(label="Add image number to file name", info="Add image number to file name")
-            save_img_grid = gr.Checkbox(label="Skip to save image grid", info="Save image grid",value= opt['skip_grid'])
-            file_format_grid = gr.Text(label="File format", placeholder="jpg", info="File format to save image grid in")
-            extended_info = gr.Checkbox(label="Add extended info (seed,prompt) to file name")
+            settings['skip_save'] = gr.Checkbox(label="Skip to save images", info="Save images",value= settings['skip_save'])
+            settings['file_format'] = gr.Text(label="File format", info="File format to save images in",value = setting['file_format'])
+            settings['image_file_pattern'] = gr.Text(label="Image file pattern", info="File pattern to save images with",value = settings['image_file_pattern'])
+            settings['add_image_number']= gr.Checkbox(label="Add image number to file name", info="Add image number to file name", value = settings['add_image_number'])
+            settings['skip_grid'] = gr.Checkbox(label="Skip to save image grid", info="Save image grid",value= settings['skip_grid'])
+            settings['file_format_grid'] = gr.Text(label="File format", info="File format to save image grid in",value = settings['file_format_grid'] )
+            settings['entended_info'] = gr.Checkbox(label="Add extended info (seed,prompt) to file name",value = settings['entended_info'])
 
         with gr.TabItem("Image paths", id="image_paths", elem_id="settings_tab_image_paths"):
-            from_file = gr.Textbox(lines=1, label="From File", placeholder="Enter a file path...", info="The file path to load the model from")
-            init_img = gr.Textbox(lines=1, label="Initial Image", placeholder="Enter a file path...", info="The file path to load the initial image from")
+            settings['from_file'] = gr.Textbox(lines=1, label="From File", info="The file path to load the model from",value = settings['from_file'])
+            settings['init_img'] = gr.Textbox(lines=1, label="Initial Image", info="The file path to load the initial image from", value = settings['init_img'])
 
+        with gr.TabItem("datasets", id="datasets"):
+            settings['laion400m'] = gr.Checkbox(label="Laion400m",value = settings['laion400m'])
+            settings['ref_prompt'] = gr.Textbox(lines=1, label="Reference Prompt", info = 'a class-level reference prompt to be mixed with the subject prompt', value = settings['ref_prompt'])
 
         with gr.TabItem("comparisons", id="comparisons", elem_id="settings_tab_comparisons"):
-            calc_face_sim = gr.Checkbox(label="Calc Face Sim")
-            compare_with = gr.Textbox(lines=1, label="Compare With", placeholder="Enter a file path...")
-
-        settings_submit.click(apply_settings, inputs=[save_img, file_format, image_file_pattern, add_image_number, save_img_grid, file_format_grid,extended_info, from_file, init_img, calc_face_sim, compare_with])
+            settings['calc_face_sim'] = gr.Checkbox(label="Calc Face Sim",value= settings['calc_face_sim'])
+            settings['compare_with'] = gr.Textbox(lines=1, label="Compare With", info="Enter a file path...",value = settings['compare_with'])
+            settings['scores_csv'] = gr.Textbox(lines=1, label="Scores CSV", info="CSV file to save the evaluation scores", value = settings['scores_csv'])
+        settings_submit.click(apply_settings)
 
 
         # with gr.TabItem("Actions", id="actions", elem_id="settings_tab_actions"):
@@ -263,5 +264,5 @@ with gr.Blocks() as demo:
     
 
 if __name__ == "__main__":
-    demo.launch(share=True)
+    demo.launch(share=True, show_error = True)
     # demo.launch()
