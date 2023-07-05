1883ecdd56fb78849e75a0acbc058931e947e1de
askerlee <shaohua@gmail.com>
2023-07-05 17:51:04 +0800

    Fix bug in deepface_embed_folder()

--- evaluation/eval_utils.py
+++ evaluation/eval_utils.py
@@ -102,18 +102,21 @@ def deepface_embed_folder(img_paths, model_name='ArcFace', detector_backend='ret
     all_embeddings = []
 
     for img_path in img_paths:
-        # img_path might have many faces
-        img_objs = deepface_functions.extract_faces(
-            img=img_path,
-            target_size=target_size,
-            detector_backend=detector_backend,
-            grayscale=False,
-            enforce_detection=enforce_detection,
-            align=align,
-        )
-
-        # --------------------------------
         embeddings = []
+
+        try:
+            # img_path might have many faces
+            img_objs = deepface_functions.extract_faces(
+                img=img_path,
+                target_size=target_size,
+                detector_backend=detector_backend,
+                grayscale=False,
+                enforce_detection=enforce_detection,
+                align=align,
+            )
+        except:
+            continue
+        
         # now we will find the face pair with minimum distance
         for img_content, img_region, _ in img_objs:
             img_embedding_obj = DeepFace.represent(
