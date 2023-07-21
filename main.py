057c465b01c3cece11278ed627614c7acd8ffb53
askerlee <shaohua@gmail.com>
2023-07-21 21:15:13 +0800

    Implemented multi-embedding token support. Removed cls_distill_token

--- main.py
+++ main.py
@@ -207,10 +207,6 @@ def get_parser(**parser_kwargs):
         type=str, default=None,
         help="A single word to use in class-level prompts for delta loss")
 
-    parser.add_argument("--cls_distill_token",
-        type=str, default=None,
-        help="A single word to use in class-level prompts for prompt distillation")
-
     # layerwise_lora_rank_token_ratio. When there are two tokens, 
     # it seems that increasing the rank to 3 doesn't help.
     parser.add_argument("--layerwise_lora_rank_token_ratio", 
@@ -718,9 +714,7 @@ if __name__ == "__main__":
 
         config.data.params.train.params.cls_delta_token      = opt.cls_delta_token
         config.data.params.validation.params.cls_delta_token = opt.cls_delta_token
-        config.data.params.train.params.cls_distill_token      = opt.cls_distill_token
-        config.data.params.validation.params.cls_distill_token = opt.cls_distill_token
-            
+
         if hasattr(opt, 'composition_regs_iter_gap'):
             config.model.params.composition_regs_iter_gap = opt.composition_regs_iter_gap
 
