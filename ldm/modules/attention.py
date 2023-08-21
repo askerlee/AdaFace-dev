5ee0ce9929b05380eb3e40bf8323de02a0747f23
askerlee <shaohua@gmail.com>
2023-08-21 13:44:35 +0800

    Reduce fg_bg_key_ortho_loss_weight by 5x

--- ldm/modules/attention.py
+++ ldm/modules/attention.py
@@ -148,7 +148,7 @@ class SpatialSelfAttention(nn.Module):
 
         return x+h_
 
-
+# All CrossAttention layers have 8 heads.
 class CrossAttention(nn.Module):
     def __init__(self, query_dim, context_dim=None, heads=8, dim_head=64, dropout=0.):
         super().__init__()
