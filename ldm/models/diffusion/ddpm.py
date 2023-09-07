c39ee2c23e7551f597c19fe494750b8a3d0ffe86
askerlee <shaohua@gmail.com>
2023-09-07 16:22:48 +0800

    Make inj_noise_t random within [0.8, 1] of t

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -1933,7 +1933,7 @@ class LatentDiffusion(DDPM):
                 # so that the noise level is sufficiently different.
                 t = torch.minimum(t_mid, t_upperbound)
                 # Inject smaller amount of noises, to retain more semantics from the previous denoising step.
-                inj_noise_t = (t * 0.8).type(t.dtype)
+                inj_noise_t = (t * np.random.uniform(0.8, 1)).type(t.dtype)
 
             else:
                 # Fresh compositional iter.
