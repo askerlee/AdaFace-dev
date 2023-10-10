e91288c52db9b37b153f84b741ee02b21476abaa
askerlee <shaohua@gmail.com>
2023-10-10 14:16:19 +0800

    Improve info

--- scripts/stable_txt2img.py
+++ scripts/stable_txt2img.py
@@ -547,6 +547,7 @@ def main(opt):
 
             if init_mask_path is not None:
                 mask_obj = Image.open(init_mask_path).convert("L")
+                print(f"Applied mask {init_mask_path}")
                 mask     = torch.from_numpy(np.array(mask_obj).astype(np.uint8)) / 255
                 mask     = F.interpolate(mask[None, None], size=(opt.H // opt.f, opt.W // opt.f), mode='nearest')
                 mask     = mask.repeat(batch_size, 1, 1, 1).to(device)
