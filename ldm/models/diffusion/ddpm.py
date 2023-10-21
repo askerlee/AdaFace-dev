f33fcea1c5a87d702b4d51822bfeaf8793cab65d
askerlee <shaohua@gmail.com>
2023-10-21 16:02:31 +0800

    Add back loss_wds_mask_align

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -2311,11 +2311,9 @@ class LatentDiffusion(DDPM):
                     loss_dict.update({f'{prefix}/fg_wds_mask_contrast': loss_fg_wds_mask_contrast.mean().detach()})
 
                 fg_wds_comple_loss_scale = 1 #0.25
-                # loss_wds_mask_align is not added, as wds embeddings are not optimizable.
-                # loss_fg_wds_mask_contrast is not added, as wds attention is not accurate.
                 loss += self.fg_wds_complementary_loss_weight \
                          * (loss_fg_wds_complementary * fg_wds_comple_loss_scale + \
-                            loss_fg_mask_align_wds + loss_fg_wds_mask_contrast)
+                            loss_fg_mask_align_wds + loss_wds_mask_align + loss_fg_wds_mask_contrast)
 
             if not self.iter_flags['use_background_token'] and not self.iter_flags['use_wds_comp']:
                 instance_fg_weights = 1
