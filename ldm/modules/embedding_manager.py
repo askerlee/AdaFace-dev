26c02535c09c52f16bf92be59f328995c99e1936
askerlee <shaohua@gmail.com>
2023-11-03 16:42:57 +0800

    Fix print bug

--- ldm/modules/embedding_manager.py
+++ ldm/modules/embedding_manager.py
@@ -1756,7 +1756,7 @@ class EmbeddingManager(nn.Module):
                             token2num_vectors[km2] = ckpt["token2num_vectors"][km]
                         print(f"Loaded {km}->{km2} from {ckpt_path}")
                         ada = self.string_to_ada_embedder_dict[km2]
-                        print(f"km2: {ada.fg_emb_count}/{ada.bg_emb_count}/{ada.K} fg/bg/total embeddings")
+                        print(f"{km2}: {ada.fg_emb_count}/{ada.bg_emb_count}/{ada.K} fg/bg/total embeddings")
 
             if "token2num_vectors" in ckpt:
                 self.set_num_vectors_per_token(token2num_vectors)
