2c188f03bf999e7e8e7fd68f1ec2fd1dea1549f2
askerlee <shaohua@gmail.com>
2023-11-14 15:33:29 +0800

    Normalize subj_bg_attn_suppress with subj_attn_mean

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -4028,13 +4028,15 @@ class LatentDiffusion(DDPM):
             # on background areas.
             subj_subj_bg_attn, mix_subj_bg_attn = subj_bg_attn.chunk(2)
 
+            subj_attn_mean = subj_attn.mean().item() + 1e-6
             # Simply suppress the subj attention on background areas. 
             # No need to use attn_*_single as references.
             # do_sqr=True: focus on large activations and tolerate more of small activations.
+            # But do_sqr will make the losses too big. Therefore we normalize them with subj_attn_mean.
             loss_layer_subj_bg_attn_suppress = masked_mean(subj_subj_bg_attn, subj_subj_bg_attn > 0,
-                                                           do_sqr=True)
+                                                           do_sqr=True) / subj_attn_mean
             loss_layer_mix_bg_attn_suppress  = masked_mean(mix_subj_bg_attn,  mix_subj_bg_attn  > 0,
-                                                           do_sqr=True)
+                                                           do_sqr=True) / subj_attn_mean
             mix_bg_attn_suppress_loss_scale = 0.2
             loss_comp_subj_bg_attn_suppress += (loss_layer_subj_bg_attn_suppress 
                                                 + loss_layer_mix_bg_attn_suppress * mix_bg_attn_suppress_loss_scale) \
