2337d2c0a752a67932ff8242467c7cecfeaa56bd
askerlee <shaohua@gmail.com>
2023-12-29 23:29:35 +0800

    Fix unaligned cls delta embs and subj embs

--- ldm/util.py
+++ ldm/util.py
@@ -951,8 +951,11 @@ def merge_cls_delta_string_embs(tokenized_text, embedded_text, placeholder_indic
                 cls_delta_string_mean_emb = (cls_delta_string_embs * init_word_weights).sum(dim=0)
                 # Set the first embedding of the cls delta tokens to the weighted sum of the cls_delta_string_embs.
                 embedded_text2[i, start] = cls_delta_string_mean_emb
-                # Set the rest of the embeddings to 0.
-                embedded_text2[i, start+1:start+M] = 0
+                # Move the embeddings (except the EOS) after the last cls delta token to the left,
+                # overwriting the rest M-1 cls delta embeddings.
+                # embedded_text2[i, -M:0] are kept unchanged.
+                # This is to align the cls delta tokens with the subject tokens in the first half of the batch.
+                embedded_text2[i, start+1:-M] = embedded_text[i, start+M:-1]
                 num_cls_strings += 1
                 break
 
