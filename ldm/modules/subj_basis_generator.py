460f4d6d9a7b9bba915b36364a6ca29661ab41c5
askerlee <shaohua@gmail.com>
2024-02-18 00:02:51 +0800

    add dataclass BaseModelOutputWithPooling2

--- ldm/modules/subj_basis_generator.py
+++ ldm/modules/subj_basis_generator.py
@@ -11,9 +11,11 @@ import torch.nn.functional as F
 from einops import rearrange
 from einops.layers.torch import Rearrange
 from transformers import CLIPVisionModel
-from transformers.modeling_outputs import BaseModelOutputWithPooling
 import numpy as np
 from torch import einsum
+from dataclasses import dataclass
+from typing import Optional, Tuple
+from transformers.utils import ModelOutput
 
 def reshape_tensor(x, heads):
     bs, length, width = x.shape
@@ -261,6 +263,38 @@ class SubjBasisGenerator(nn.Module):
         latent_queries = self.proj_out(latent_queries)
         return self.norm_out(latent_queries)
 
+@dataclass
+class BaseModelOutputWithPooling2(ModelOutput):
+    """
+    Base class for model's outputs that also contains a pooling of the last hidden states.
+
+    Args:
+        last_hidden_state (`torch.FloatTensor` of shape `(batch_size, sequence_length, hidden_size)`):
+            Sequence of hidden-states at the output of the last layer of the model.
+        pooler_output (`torch.FloatTensor` of shape `(batch_size, hidden_size)`):
+            Last layer hidden-state of the first token of the sequence (classification token) after further processing
+            through the layers used for the auxiliary pretraining task. E.g. for BERT-family of models, this returns
+            the classification token after processing through a linear layer and a tanh activation function. The linear
+            layer weights are trained from the next sentence prediction (classification) objective during pretraining.
+        hidden_states (`tuple(torch.FloatTensor)`, *optional*, returned when `output_hidden_states=True` is passed or when `config.output_hidden_states=True`):
+            Tuple of `torch.FloatTensor` (one for the output of the embeddings, if the model has an embedding layer, +
+            one for the output of each layer) of shape `(batch_size, sequence_length, hidden_size)`.
+
+            Hidden-states of the model at the output of each layer plus the optional initial embedding outputs.
+        attentions (`tuple(torch.FloatTensor)`, *optional*, returned when `output_attentions=True` is passed or when `config.output_attentions=True`):
+            Tuple of `torch.FloatTensor` (one for each layer) of shape `(batch_size, num_heads, sequence_length,
+            sequence_length)`.
+
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+    """
+
+    last_hidden_state: torch.FloatTensor = None
+    pooler_output: torch.FloatTensor = None
+    hidden_states: Optional[Tuple[torch.FloatTensor]] = None
+    attentions: Optional[Tuple[torch.FloatTensor]] = None
+    attn_mask: Optional[torch.FloatTensor] = None
+
 # Revised from CLIPVisionTransformer. self: a CLIPVisionTransformer instance.
 # https://github.com/huggingface/transformers/blob/main/src/transformers/models/clip/modeling_clip.py#L821
 # pixel_values: preprocessed B*C*H*W images. [BS, 3, 224, 224]
@@ -323,7 +357,7 @@ def CLIPVisionTransformer_forward(self, pixel_values = None, attn_mask=None,
         if not return_dict:
             return (last_hidden_state, pooled_output) + encoder_outputs[1:]
 
-        return BaseModelOutputWithPooling(
+        return BaseModelOutputWithPooling2(
             last_hidden_state=last_hidden_state,
             pooler_output=pooled_output,
             hidden_states=encoder_outputs.hidden_states,
