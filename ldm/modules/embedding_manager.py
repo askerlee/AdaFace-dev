Fix wrong dim bug

ldm/modules/embedding_manager.py
@@ -495,7 +495,8 @@ class StaticLayerwiseEmbedding(nn.Module):
         print(f"{zero_shot_sig} StaticLayerwiseEmbedding {token_string} initialized with {self.K} total embs, {self.N} init vectors ({init_string}), {self.r} basis vectors")
 
     # Return static embeddings of all layers together.
-    # zs_basis_vecs: [K, r, 768]. K: number of vectors per token. r: number of basis vectors for each vector.
+    # static_zs_embs: [16, K, 768]. 
+    # 16: number of layers. K: number of vectors per token. 
     def forward(self, static_zs_embs=None):
         with torch.autocast(device_type=self.device_type, enabled=False):
             # self.basis_comm_weights: [1, K, r] broadcasted to [16, K, r].

ldm/modules/subj_basis_generator.py
@@ -327,7 +327,7 @@ def CLIPVisionTransformer_forward(self, pixel_values = None, attn_mask=None,
             # Prepend 1 to the mask: [BS, 1, 256] => [BS, 1, 257]. 
             # This 1 corresponds to class_embeds, which is always attended to.
             attn_mask = torch.cat([torch.ones(*attn_mask.shape[:2], 1).to(attn_mask.device), attn_mask], dim=-1)
-            attn_mask_pairs = torch.matmul(attn_mask.transpose(-1, -2), attn_mask).bool().unsqueeze(1)
+            attn_mask_pairs = torch.matmul(attn_mask.transpose(-1, -2), attn_mask)
         else:
             attn_mask_pairs = None
 

ldm/util.py
@@ -2268,7 +2268,7 @@ def encode_image_fg_bg_with_clip(images, fg_masks):
         if isinstance(fg_masks, (list, tuple)):
             fg_masks2 = []
             for fg_mask in fg_masks:
-                # fg_mask2: [Hi, Wi]
+                # fg_mask: [Hi, Wi]
                 # BUG: clip_preprocessor will do central crop on images. But fg_mask is not central cropped.
                 # If the ref image is not square, then the fg_mask will not match the image.
                 # TODO: crop fg_mask and images to square before calling encode_image_fg_bg_with_clip().
@@ -2283,11 +2283,13 @@ def encode_image_fg_bg_with_clip(images, fg_masks):
             # The actual size doesn't matter, 
             # as fg_mask2 will be resized to the same size as image features 
             # (much smaller than image_pixel_values).            
-            fg_masks2 = torch.tensor(fg_masks, device=clip_device).float()
+            fg_masks2 = torch.tensor(fg_masks, device=clip_device).float().unsqueeze(1)
+            fg_masks2 = F.interpolate(fg_masks2, size=image_pixel_values.shape[-2:], mode='bilinear', align_corners=False)
+            fg_masks2 = fg_masks2.squeeze(1)
     else:
         # fg_mask2: [BS, 224, 224]. 
         fg_masks2 = torch.ones_like(image_pixel_values[:, 0, :, :], device=clip_device)
-        
+
     with torch.no_grad():
         if neg_image_features is None:
             # neg_pixel_values: [1, 3, 224, 224]