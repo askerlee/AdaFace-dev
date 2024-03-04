f9ddbe26aaa2b627e7248125b63284731bc1b61f
askerlee <shaohua@gmail.com>
2024-03-04 08:32:19 +0800

    reduce codebook size

--- ldm/modules/subj_basis_generator.py
+++ ldm/modules/subj_basis_generator.py
@@ -275,7 +275,7 @@ class SubjBasisGenerator(nn.Module):
         max_seq_len: int = 257,             # [CLS token, image tokens]
         apply_pos_emb: bool = True,         # Newer IP Adapter uses positional embeddings.
         elementwise_affine: bool = True,    # Whether to use elementwise affine in LayerNorms.
-        codebook_size: int = 2340,          # Size of the codebook, 50 * num_out_queries.
+        codebook_size: int = 234,          # Size of the codebook, 50 * num_out_queries.
         use_FFN: bool = True,               # Whether to use FeedForward layer after cross-attention.
         placeholder_is_bg: bool = False,    # Whether the placeholder is for the image background.
     ):
