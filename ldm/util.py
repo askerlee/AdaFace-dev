b872ab8f7367c95ff33439dba05f2271535c6df4
askerlee <shaohua@gmail.com>
2024-03-13 17:20:37 +0800

    Make GradientScaler compatible with older ckpts

--- ldm/util.py
+++ ldm/util.py
@@ -1124,6 +1124,10 @@ class GradientScaler(nn.Module):
         self._debug = torch.tensor(debug, requires_grad=False)
 
     def forward(self, input_):
+        if hasattr('_debug', self):
+            _debug = self._debug
+        else:
+            _debug = False
         return ScaleGrad.apply(input_, self._alpha.to(input_.device), self._debug)
 
 def gen_gradient_scaler(alpha, debug=False):
