b08d7a058e7df590ed392b864b097d9573899356
askerlee <shaohua@gmail.com>
2024-04-08 13:20:27 +0800

    Add script to trash bad images

---
+++ scripts-private/gen_move_trash.sh
@@ -0,0 +1,25 @@
+mkdir -p /data/shaohua/trash
+
+while read -l file_path
+    # /data/shaohua/VGGface2_HQ_masks/n001154/0342_01.jpg -> /data/shaohua/trash/n001154-0342_01.jpg
+    set trash_path (string replace "VGGface2_HQ_masks" "trash" $file_path)
+    set trash_path (string replace "/0" -- "-0" $trash_path)
+    echo $file_path - $trash_path >&2
+    if test -e $trash_path
+        echo "Duplicate file: $file_path" >&2
+        continue
+    end
+
+    cp $file_path $trash_path
+
+    # Extract the directory path
+    set dir_path (dirname $file_path)
+
+    # Replace the part of the path to form the new directory path
+    set new_dir_path (string replace "VGGface2_HQ_masks" "VGGface2_HQ_masks_trash" $dir_path)
+
+    # Create the new directory (if it doesn't exist)
+    echo mkdir $new_dir_path
+    # Move the file to the new directory
+    echo mv $file_path $new_dir_path/(basename $file_path)
+end
