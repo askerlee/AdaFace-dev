2f0d90a9dfdb001a138dd39ca9a0c0680ace7620
askerlee <shaohua@gmail.com>
2024-04-24 11:30:55 +0800

    Avoid initializing datasets twice

--- main.py
+++ main.py
@@ -993,7 +993,7 @@ if __name__ == "__main__":
         # NOTE according to https://pytorch-lightning.readthedocs.io/en/latest/datamodules.html
         # calling these ourselves should not be necessary but it is.
         # lightning still takes care of proper multiprocessing though
-        data.prepare_data()
+        # data.prepare_data()
         data.setup()
 
         print("#### Data #####")
