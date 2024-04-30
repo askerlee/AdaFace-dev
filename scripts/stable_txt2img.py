0ac9db7cbbf5e383af6a4100edd3836dc92741f6
askerlee <shaohua@gmail.com>
2024-04-30 14:55:32 +0800

    Default zs_arc2face_inverse_prompt_embs_inf_type change to full_zeroed_extra

--- scripts/stable_txt2img.py
+++ scripts/stable_txt2img.py
@@ -282,7 +282,7 @@ def parse_args():
                         help="Apply negative subject bases for zero-shot learning")
     parser.add_argument("--zs_cls_delta_string", type=str, default=None,
                         help="Class delta string for zero-shot learning")
-    parser.add_argument("--zs_arc2face_inverse_prompt_embs_inf_type", type=str, default='full',
+    parser.add_argument("--zs_arc2face_inverse_prompt_embs_inf_type", type=str, default='full_zeroed_extra',
                         choices=['full_zeroed_extra', 'full', 'full_avg_pad', 'full_pad', 'b_core_e'],
                         help="Inverse prompt embeddings type during inference under zero-shot learning")
     
