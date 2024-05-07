329e3107844494f35afc662ae585bf355790ecba
askerlee <shaohua@gmail.com>
2024-05-07 21:14:41 +0800

    prompt_translator outputs a learnable combination of the last 5 layer features

--- ldm/modules/subj_basis_generator.py
+++ ldm/modules/subj_basis_generator.py
@@ -446,7 +446,8 @@ class SubjBasisGenerator(nn.Module):
                                 out_has_skip=out_has_skip)
         else:
             self.prompt_translator = CLIPTextModel.from_pretrained('openai/clip-vit-large-patch14').text_model.encoder
-
+            self.num_pt_last_layers     = 5
+            self.pt_last_layers_weights = nn.Parameter(torch.ones(self.num_pt_last_layers), requires_grad=True)
         print(repr(self))
 
     # list_extra_words: a list of length BS. Each element is a string of extra words.
@@ -517,8 +518,20 @@ class SubjBasisGenerator(nn.Module):
         else:
             # If fg, we use a CLIP-encoder to generate 77 embs, and we take the first 16*4=64.
             # id_embs: [BS, 77, 768]. id_embs_out: [BS, 77, 768].
-            id_embs_out = self.prompt_translator(inputs_embeds=id_embs, return_dict=False)[0]
-            # NOTE: this fix is no longer necessary as the training progresses,
+            id_embs_out, pt_last_hidden_states = self.prompt_translator(inputs_embeds=id_embs, output_hidden_states=True, return_dict=False)
+            pt_last_hidden_states = pt_last_hidden_states[-self.num_pt_last_layers:]
+            pt_last_layers_weights = self.pt_last_layers_weights.to(pt_last_hidden_states[0].dtype)
+            # Normalize the weights of to sum to 1 across layers.
+            # hidden_state_layer_weights: [3, 1] or [3, 768].
+            pt_last_layers_weights = pt_last_layers_weights / pt_last_layers_weights.sum(dim=0, keepdim=True)
+            # [3, 1/768] -> [3, 1, 1, 1/768]
+            pt_last_layers_weights = pt_last_layers_weights.unsqueeze(1).unsqueeze(1)
+            # A weighted sum of pt_last_hidden_states.
+            # [3, 1, 22, 768] * [3, 1, 1, 1/768] -> [3, 1, 22, 768] -> [1, 22, 768]
+            pt_last_hidden_state = (torch.stack(pt_last_hidden_states, dim=0) * pt_last_layers_weights).sum(dim=0)
+            id_embs_out = pt_last_hidden_state
+
+            # NOTE: the fix below is no longer necessary as the training progresses,
             # it seems that the model learns to repurpose BOS as one of the identity embeddings.
             # Replace the first token, i.e., the BOS token, which has 
             # very large embedding values and will take up too much attention.
