816bb4f9a7eeec1efd85fd6c640734adb7de4f0a
askerlee <shaohua@gmail.com>
2024-05-15 22:55:50 +0800

    Add nsfw words to negative prompt

--- scripts/stable_txt2img.py
+++ scripts/stable_txt2img.py
@@ -368,7 +368,10 @@ def main(opt):
     # More complex negative prompts may hurt the performance.
     # predefined_negative_prompt = "duplicate, out of frame, cropped, mutilated, bad anatomy, deformed, bad proportions, disfigured, mutation"
     # This negative prompt is borrowed from PuLID.
-    predefined_negative_prompt = "flaws in the eyes, flaws in the face, lowres, non-HDRi, low quality, worst quality, artifacts, noise, text, watermark, glitch, mutated, ugly, disfigured, hands, partially rendered objects, partially rendered eyes, deformed eyeballs, cross-eyed, blurry, mutation, duplicate, out of frame, cropped, mutilated, bad anatomy, deformed, bad proportions"
+    predefined_negative_prompt = "flaws in the eyes, flaws in the face, lowres, non-HDRi, low quality, worst quality, artifacts, noise, text, watermark, glitch, " \
+                                 "mutated, ugly, disfigured, hands, partially rendered objects, partially rendered eyes, deformed eyeballs, cross-eyed, blurry, " \
+                                 "mutation, duplicate, out of frame, cropped, mutilated, bad anatomy, deformed, bad proportions, " \
+                                 "nude, naked, nsfw, topless, bare breasts"
     device = f"cuda:{opt.gpu}" if torch.cuda.is_available() else "cpu"
     
     if not opt.eval_blip:
