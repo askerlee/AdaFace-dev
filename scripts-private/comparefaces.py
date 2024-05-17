92e8a74a624c7d3dfea239ec9a2fedc3e550d35a
askerlee <shaohua@gmail.com>
2024-05-17 22:59:45 +0800

    Update comparefaces.py

--- scripts-private/comparefaces.py
+++ scripts-private/comparefaces.py
@@ -7,12 +7,12 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("path1", type=str, help="path1")
     parser.add_argument("path2", type=str, nargs='?', default=None, help="path2")
-    # --self
     parser.add_argument("--self", dest="self_comp", action="store_true", help="self comparison")
-    # --pair
     parser.add_argument("--pair", dest="folderpair_comp", action="store_true", help="subfolder pairwise comparison")
-    # --gpu
     parser.add_argument("--gpu", dest='gpu_id', type=int, default=0, help="specify a gpu to use")
+    parser.add_argument("--face_engine", dest='face_engine', type=str, default='deepface', 
+                        choices=['deepface', 'insightface'],
+                        help="face engine to use for comparison")
     args = parser.parse_args()
     return args
 
@@ -36,13 +36,13 @@ if __name__ == "__main__":
                     continue
 
                 print("Self comparing %s:" %(subdir_path))
-                compare_face_folders_fast(subdir_path, subdir_path)
+                compare_face_folders_fast(subdir_path, subdir_path, face_engine=args.face_engine)
                 subdir_count += 1
             if subdir_count == 0:
                 print("Self comparing %s:" %(args.path1))
-                compare_face_folders_fast(args.path1, args.path1)
+                compare_face_folders_fast(args.path1, args.path1, face_engine=args.face_engine)
         else:            
-            compare_face_folders_fast(args.path1, args.path2)
+            compare_face_folders_fast(args.path1, args.path2, face_engine=args.face_engine)
     else:
         subdirs = sorted(os.listdir(args.path1))
         for subdir in subdirs:
@@ -53,7 +53,7 @@ if __name__ == "__main__":
             if os.path.isdir(subdir_path1) == False or os.path.isdir(subdir_path2) == False:
                 continue
             print("Pair comparing %s vs %s:" %(subdir_path1, subdir_path2))
-            compare_face_folders_fast(subdir_path1, subdir_path2)        
+            compare_face_folders_fast(subdir_path1, subdir_path2, face_engine=args.face_engine)     
         
     end = time.time()
     print("Time elapsed: %.2f seconds" %(end - begin))
