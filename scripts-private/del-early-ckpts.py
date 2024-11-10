577ed0d82906d732d1ce1d7c49209102f5fd4a93
askerlee <shaohua@gmail.com>
2024-11-10 11:48:57 +0800

    Fix bugs in LDM inference, update del-early-ckpts.py to include a filtering pattern

--- scripts-private/del-early-ckpts.py
+++ scripts-private/del-early-ckpts.py
@@ -1,7 +1,8 @@
 import os
 import re
+import sys
 
-def find_largest_checkpoint(folder_path):
+def delete_all_but_largest_checkpoint(folder_path):
     # Regex to match checkpoint files like embeddings_gs-5500.pt and extract the iteration number
     checkpoint_pattern = re.compile(r'embeddings_gs-(\d+)\.pt')
     
@@ -30,14 +31,15 @@ def find_largest_checkpoint(folder_path):
     
     print(f"Kept:    {os.path.join(folder_path, largest_checkpoint)}")
 
-def clean_log_folders(root_log_folder):
+def clean_log_folders(root_log_folder, pat):
     # Iterate through all subfolders (log folders)
     for subfolder in os.listdir(root_log_folder):
         folder_path = os.path.join(root_log_folder, subfolder, 'checkpoints')
-        if os.path.isdir(folder_path):
-            find_largest_checkpoint(folder_path)
+        if os.path.isdir(folder_path) and pat in folder_path:
+            delete_all_but_largest_checkpoint(folder_path)
 
 if __name__ == "__main__":
     # Set the path to the root folder containing all log folders
     root_log_folder = "logs/"  # Update this path
-    clean_log_folders(root_log_folder)
+    ckpt_pat = sys.argv[1]
+    clean_log_folders(root_log_folder, pat=ckpt_pat)
