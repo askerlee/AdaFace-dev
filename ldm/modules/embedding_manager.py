ea9853975bdfc1ef1da3fd3af418ef36087c755c
askerlee <shaohua@gmail.com>
2024-12-22 01:08:44 +0800

    Separate load_unet_loras_from_ckpt into load_unet_attn_lora_from_ckpt and load_unet_attn_lora_from_ckpt

--- ldm/modules/embedding_manager.py
+++ ldm/modules/embedding_manager.py
@@ -61,7 +61,8 @@ class EmbeddingManager(nn.Module):
             gen_ss_from_frozen_subj_basis_generator=False,
             multi_token_filler=',',
             unet_lora_modules=None,
-            load_unet_loras_from_ckpt=True,
+            load_unet_attn_lora_from_ckpt=True,
+            load_unet_ffn_lora_from_ckpt=True,
     ):
         super().__init__()
 
@@ -181,7 +182,7 @@ class EmbeddingManager(nn.Module):
         # So we don't pass adafaec_ckpt_paths to create_id2ada_prompt_encoder(), 
         # but instead use self.load().
         if adaface_ckpt_paths is not None:
-            self.load(adaface_ckpt_paths, load_unet_loras_from_ckpt)
+            self.load(adaface_ckpt_paths, load_unet_attn_lora_from_ckpt, load_unet_ffn_lora_from_ckpt)
 
         # Initialize self.subj_name_to_cls_delta_tokens.
         self.init_cls_delta_tokens(self.get_tokens_for_string, subj_name_to_cls_delta_string, cls_delta_string)
@@ -581,7 +582,7 @@ class EmbeddingManager(nn.Module):
         torch.save(saved_dict, adaface_ckpt_path)
 
     # Load custom tokens and their learned embeddings from "embeddings_gs-4500.pt".
-    def load(self, adaface_ckpt_paths, load_unet_loras_from_ckpt=True):
+    def load(self, adaface_ckpt_paths, load_unet_attn_lora_from_ckpt=True, load_unet_ffn_lora_from_ckpt=True):
         # The default placeholder specified in the config file will be loaded to these dicts.
         # So before loading, remove it from these dicts first.
         self.string_to_token_dict   = {}
@@ -647,9 +648,16 @@ class EmbeddingManager(nn.Module):
                     self.string_to_token_dict[km2] = k2_token
                     print(f"Loaded {km}->{km2} from {adaface_ckpt_path}")
                 
-            if self.unet_lora_modules is not None and load_unet_loras_from_ckpt \
-              and 'unet_lora_modules' in ckpt:
+            if self.unet_lora_modules is not None and 'unet_lora_modules' in ckpt \
+              and (load_unet_attn_lora_from_ckpt or load_unet_ffn_lora_from_ckpt):
                 unet_lora_modules = ckpt['unet_lora_modules']
+                if not load_unet_attn_lora_from_ckpt:
+                    # Filter out the attention LoRA modules.
+                    unet_lora_modules = { k: v for k, v in unet_lora_modules.items() if 'attn2_processor' not in k }
+                if not load_unet_ffn_lora_from_ckpt:
+                    # Filter out the FFN LoRA modules.
+                    unet_lora_modules = { k: v for k, v in unet_lora_modules.items() if 'resnets' not in k }
+
                 ret = self.unet_lora_modules.load_state_dict(unet_lora_modules, strict=False)
                 print(f"Loaded {len(unet_lora_modules)} LoRA weights")
 
@@ -663,7 +671,7 @@ class EmbeddingManager(nn.Module):
                 continue
             elif 'unet_lora_modules' not in ckpt:
                 print(f"'unet_lora_modules' not found in {adaface_ckpt_path}")
-            elif not load_unet_loras_from_ckpt:
+            elif not load_unet_attn_lora_from_ckpt and not load_unet_ffn_lora_from_ckpt:
                 print(f"Instructed to skip loading unet_lora_modules from {adaface_ckpt_path}")
 
         # ',' is used as filler tokens.
